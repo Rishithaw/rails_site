5ec0a90125df95a43a6a43ad8d289f4e7c91e01c
rishithaw <rishithaw1@gmail.com>
2025-11-09 20:37:51 -0600

    Fixed method name issue

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -3,7 +3,7 @@ class Dog < ApplicationRecord
   belongs_to :sub_breed
 
   has_many :dogs_traits, dependent: :destroy
-  has_many :traits, through: :dogs_traits
+  has_many :traits, through: :dog_traits
 
   validates :name, presence: true
   validates :sub_breed_id, presence: true
